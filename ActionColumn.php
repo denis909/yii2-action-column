55bb54703f9926fd776051a51fa32ad9c2c9640c
denis909 <denis909@mail.ru>
2020-02-24 15:05:31 +0300

    n/a

--- ActionColumn.php
+++ ActionColumn.php
@@ -2,6 +2,8 @@
 
 namespace denis909\yii;
 
+use Yii;
+use yii\helpers\Html;
 use yii\helpers\Url;
 
 class ActionColumn extends \yii\grid\ActionColumn
@@ -12,6 +14,44 @@ class ActionColumn extends \yii\grid\ActionColumn
     public $options = ['style' => 'width: 1%'];
 
     public $baseUrl;
+
+    public $enableIcons = true;
+
+    protected function initDefaultButton($name, $iconName, $additionalOptions = [])
+    {
+        if (!$this->enableIcons)
+        {
+            if (!isset($this->buttons[$name]) && strpos($this->template, '{' . $name . '}') !== false)
+            {
+                $this->buttons[$name] = function ($url, $model, $key) use ($name, $iconName, $additionalOptions)
+                {
+                    switch ($name) {
+                        case 'view':
+                            $title = Yii::t('yii', 'View');
+                            break;
+                        case 'update':
+                            $title = Yii::t('yii', 'Update');
+                            break;
+                        case 'delete':
+                            $title = Yii::t('yii', 'Delete');
+                            break;
+                        default:
+                            $title = ucfirst($name);
+                    }
+                
+                    $options = array_merge([
+                        'title' => $title,
+                        'aria-label' => $title,
+                        'data-pjax' => '0',
+                    ], $additionalOptions, $this->buttonOptions);
+                
+                    return Html::a($title, $url, $options);
+                };
+            }
+        }
+
+        return parent::initDefaultButton($name, $iconName, $additionalOptions);
+    }
     
     public function createUrl($action, $model, $key, $index)
     {
